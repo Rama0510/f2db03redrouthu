6df1de17d75854f0220cdcaf53989670b1c60668
Rama0510 <111701613+Rama0510@users.noreply.github.com>
2022-11-18 01:16:14 -0600

    added all the files

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  } 
+)); 
 
 require('dotenv').config();
 const connectionString =
@@ -14,22 +31,13 @@ mongoose.connect(connectionString,
     useUnifiedTopology: true
   });
 
-//Get the default connection 
-var db = mongoose.connection;
-
-//Bind connection to error event  
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function () {
-  console.log("Connection to DB succeeded")
-});
-
-var Car = require('./models/car');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var carRouter = require('./routes/car');
 var gridbuildRouter = require('./routes/gridbuild');
 var selectorRouter = require('./routes/selector');
-var resourceRouter = require('./routes/resource');
+var resourceRouter = require("./routes/resource");
+var car = require("./models/car");
 
 var app = express();
 
@@ -37,45 +45,64 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/cars', carRouter);
+app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selectorRouter);
+app.use('/resource', resourceRouter);
+
+
 async function recreateDB() {
   // Delete everything 
-  await Car.deleteMany();
+  await car.deleteMany();
   let instance1 = new
-    Car({"car_name": "BMW","car_color": "red", "car_size": 100 });
+    car({"car_name": "BMW","car_color": "red", "car_size": 100 });
   instance1.save(function (err, doc) {
     if (err) return console.error(err);
     console.log("First object saved")
   });
   let instance2 = new
-    Car({ "car_name": "KIA", "car_color": "blue", "car_size": 100 });
+    car({ "car_name": "KIA", "car_color": "blue", "car_size": 100 });
   instance2.save(function (err, doc) {
     if (err) return console.error(err);
     console.log("Second object saved")
   });
   let instance3 = new
-    Car({ "car_name": "Honda", "car_color": "silver", "car_size": 150 });
+    car({ "car_name": "Honda", "car_color": "silver", "car_size": 150 });
   instance3.save(function (err, doc) {
     if (err) return console.error(err);
     console.log("Third object saved")
   });
 }
 
+
 let reseed = true;
 if (reseed) { recreateDB(); }
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/car', carRouter);
-app.use('/gridbuild', gridbuildRouter);
-app.use('/selector', selectorRouter);
-app.use('/resource', resourceRouter);
-
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
